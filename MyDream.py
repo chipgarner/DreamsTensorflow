727bd952ab9b62c4d98de49c0fee5f1a21472867
jkg <chip@garnertotalenergy.com>
2017-08-20 10:10:01 -0700

    Removed extra code, full screen display

--- MyDream.py
+++ MyDream.py
@@ -1,9 +1,6 @@
 # boilerplate code
 from __future__ import print_function
-import os
-from io import BytesIO
 import numpy as np
-from functools import partial
 import PIL.Image
 from IPython.display import clear_output, Image, display, HTML
 import cv2
@@ -93,8 +90,8 @@ channel = 139  # picking some feature channel to visualize
 # start with a gray image with a little noise
 img_noise = np.random.uniform(size=(224, 224, 3)) + 100.0
 
-cv2.namedWindow("Show", cv2.WINDOW_NORMAL) #cv2.WND_PROP_FULLSCREEN)
-cv2.setWindowProperty("Show", 1200, 1200) #cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+cv2.namedWindow("Show", cv2.WND_PROP_FULLSCREEN)
+cv2.setWindowProperty("Show", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
 
 
 def showarray(a, fmt='jpeg'):
@@ -111,24 +108,6 @@ def visstd(a, s=0.1):
 def T(layer):
     '''Helper for getting layer output tensor'''
     return graph.get_tensor_by_name("import/%s:0" % layer)
-
-
-# def render_naive(t_obj, img0=img_noise, iter_n=20, step=1.0):
-#     t_score = tf.reduce_mean(t_obj)  # defining the optimization objective
-#     t_grad = tf.gradients(t_score, t_input)[0]  # behold the power of automatic differentiation!
-#
-#     img = img0.copy()
-#     for i in range(iter_n):
-#         g, score = sess.run([t_grad, t_score], {t_input: img})
-#         # normalizing the gradient, so the same step size should work
-#         g /= g.std() + 1e-8  # for different layers and networks
-#         img += g * step
-#         print(score, end=' ')
-#     clear_output()
-#     showarray(visstd(img))
-#
-#
-# render_naive(T(layer)[:, :, :, channel])
 
 
 def tffunc(*argtypes):
@@ -165,101 +144,6 @@ def calc_grad_tiled(img, t_grad, tile_size=512):
             g = sess.run(t_grad, {t_input:sub})
             grad[y:y+sz,x:x+sz] = g
     return np.roll(np.roll(grad, -sx, 1), -sy, 0)
-
-#
-# def render_multiscale(t_obj, img0=img_noise, iter_n=10, step=1.0, octave_n=3, octave_scale=1.4):
-#     t_score = tf.reduce_mean(t_obj)  # defining the optimization objective
-#     t_grad = tf.gradients(t_score, t_input)[0]  # behold the power of automatic differentiation!
-#
-#     img = img0.copy()
-#     for octave in range(octave_n):
-#         if octave > 0:
-#             hw = np.float32(img.shape[:2]) * octave_scale
-#             img = resize(img, np.int32(hw))
-#         for i in range(iter_n):
-#             g = calc_grad_tiled(img, t_grad)
-#             # normalizing the gradient, so the same step size should work
-#             g /= g.std() + 1e-8  # for different layers and networks
-#             img += g * step
-#             print('.', end=' ')
-#         clear_output()
-#         showarray(visstd(img))
-#
-#
-# render_multiscale(T(layer)[:, :, :, channel])
-
-# k = np.float32([1,4,6,4,1])
-# k = np.outer(k, k)
-# k5x5 = k[:,:,None,None]/k.sum()*np.eye(3, dtype=np.float32)
-#
-# def lap_split(img):
-#     '''Split the image into lo and hi frequency components'''
-#     with tf.name_scope('split'):
-#         lo = tf.nn.conv2d(img, k5x5, [1,2,2,1], 'SAME')
-#         lo2 = tf.nn.conv2d_transpose(lo, k5x5*4, tf.shape(img), [1,2,2,1])
-#         hi = img-lo2
-#     return lo, hi
-#
-# def lap_split_n(img, n):
-#     '''Build Laplacian pyramid with n splits'''
-#     levels = []
-#     for i in range(n):
-#         img, hi = lap_split(img)
-#         levels.append(hi)
-#     levels.append(img)
-#     return levels[::-1]
-#
-# def lap_merge(levels):
-#     '''Merge Laplacian pyramid'''
-#     img = levels[0]
-#     for hi in levels[1:]:
-#         with tf.name_scope('merge'):
-#             img = tf.nn.conv2d_transpose(img, k5x5*4, tf.shape(hi), [1,2,2,1]) + hi
-#     return img
-#
-# def normalize_std(img, eps=1e-10):
-#     '''Normalize image by making its standard deviation = 1.0'''
-#     with tf.name_scope('normalize'):
-#         std = tf.sqrt(tf.reduce_mean(tf.square(img)))
-#         return img/tf.maximum(std, eps)
-#
-# def lap_normalize(img, scale_n=4):
-#     '''Perform the Laplacian pyramid normalization.'''
-#     img = tf.expand_dims(img,0)
-#     tlevels = lap_split_n(img, scale_n)
-#     tlevels = list(map(normalize_std, tlevels))
-#     out = lap_merge(tlevels)
-#     return out[0,:,:,:]
-
-# # Showing the lap_normalize graph with TensorBoard
-# lap_graph = tf.Graph()
-# with lap_graph.as_default():
-#     lap_in = tf.placeholder(np.float32, name='lap_in')
-#     lap_out = lap_normalize(lap_in)
-# show_graph(lap_graph)
-#
-# def render_lapnorm(t_obj, img0=img_noise, visfunc=visstd,
-#                    iter_n=10, step=1.0, octave_n=3, octave_scale=1.4, lap_n=4):
-#     t_score = tf.reduce_mean(t_obj) # defining the optimization objective
-#     t_grad = tf.gradients(t_score, t_input)[0] # behold the power of automatic differentiation!
-#     # build the laplacian normalization graph
-#     lap_norm_func = tffunc(np.float32)(partial(lap_normalize, scale_n=lap_n))
-#
-#     img = img0.copy()
-#     for octave in range(octave_n):
-#         if octave>0:
-#             hw = np.float32(img.shape[:2])*octave_scale
-#             img = resize(img, np.int32(hw))
-#         for i in range(iter_n):
-#             g = calc_grad_tiled(img, t_grad)
-#             g = lap_norm_func(g)
-#             img += g*step
-#             print('.', end = ' ')
-#         clear_output()
-#         showarray(visfunc(img))
-#
-# render_lapnorm(T(layer)[:,:,:,channel])
-# render_lapnorm(T(layer)[:,:,:,65])
 
 
 def render_deepdream(t_obj, img0=img_noise,
