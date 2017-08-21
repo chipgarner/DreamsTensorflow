ac75afd0667b4a5c09633f310bf82f787394eb00
jkg <chip@garnertotalenergy.com>
2017-08-21 16:42:57 -0700

    Randomly selects from 5 images, clean up

--- MyDream.py
+++ MyDream.py
@@ -4,7 +4,6 @@ import random
 import cv2
 import tensorflow as tf
 
-
 model_fn = 'inception5h/tensorflow_inception_graph.pb'
 
 # creating TensorFlow session and loading the model
@@ -13,10 +12,10 @@ sess = tf.InteractiveSession(graph=graph)
 with tf.gfile.FastGFile(model_fn, 'rb') as f:
     graph_def = tf.GraphDef()
     graph_def.ParseFromString(f.read())
-t_input = tf.placeholder(np.float32, name='input') # define the input tensor
+t_input = tf.placeholder(np.float32, name='input')  # define the input tensor
 imagenet_mean = 117.0
-t_preprocessed = tf.expand_dims(t_input-imagenet_mean, 0)
-tf.import_graph_def(graph_def, {'input':t_preprocessed})
+t_preprocessed = tf.expand_dims(t_input - imagenet_mean, 0)
+tf.import_graph_def(graph_def, {'input': t_preprocessed})
 
 layers = [op.name for op in graph.get_operations() if op.type == 'Conv2D' and 'import/' in op.name]
 feature_nums = [int(graph.get_tensor_by_name(name + ':0').get_shape()[-1]) for name in layers]
@@ -25,61 +24,60 @@ print('Number of layers', len(layers))
 print('Total number of feature channels:', sum(feature_nums))
 
 
-# Helper functions for TF Graph visualization
-
-def strip_consts(graph_def, max_const_size=32):
-    """Strip large constant values from graph_def."""
-    strip_def = tf.GraphDef()
-    for n0 in graph_def.node:
-        n = strip_def.node.add()
-        n.MergeFrom(n0)
-        if n.op == 'Const':
-            tensor = n.attr['value'].tensor
-            size = len(tensor.tensor_content)
-            if size > max_const_size:
-                tensor.tensor_content = tf.compat.as_bytes("<stripped %d bytes>" % size)
-    return strip_def
-
-
-def rename_nodes(graph_def, rename_func):
-    res_def = tf.GraphDef()
-    for n0 in graph_def.node:
-        n = res_def.node.add()
-        n.MergeFrom(n0)
-        n.name = rename_func(n.name)
-        for i, s in enumerate(n.input):
-            n.input[i] = rename_func(s) if s[0] != '^' else '^' + rename_func(s[1:])
-    return res_def
-
-
-def show_graph(graph_def, max_const_size=32):
-    """Visualize TensorFlow graph."""
-    if hasattr(graph_def, 'as_graph_def'):
-        graph_def = graph_def.as_graph_def()
-    strip_def = strip_consts(graph_def, max_const_size=max_const_size)
-    code = """
-        <script>
-          function load() {{
-            document.getElementById("{id}").pbtxt = {data};
-          }}
-        </script>
-        <link rel="import" href="https://tensorboard.appspot.com/tf-graph-basic.build.html" onload=load()>
-        <div style="height:600px">
-          <tf-graph-basic id="{id}"></tf-graph-basic>
-        </div>
-    """.format(data=repr(str(strip_def)), id='graph' + str(np.random.rand()))
-
-    iframe = """
-        <iframe seamless style="width:800px;height:620px;border:0" srcdoc="{}"></iframe>
-    """.format(code.replace('"', '&quot;'))
-    # display(HTML(iframe))
-
-
-
-# Visualizing the network graph. Be sure expand the "mixed" nodes to see their
-# internal structure. We are going to visualize "Conv2D" nodes.
-tmp_def = rename_nodes(graph_def, lambda s: "/".join(s.split('_', 1)))
-show_graph(tmp_def)
+# # Helper functions for TF Graph visualization
+#
+# def strip_consts(graph_def, max_const_size=32):
+#     """Strip large constant values from graph_def."""
+#     strip_def = tf.GraphDef()
+#     for n0 in graph_def.node:
+#         n = strip_def.node.add()
+#         n.MergeFrom(n0)
+#         if n.op == 'Const':
+#             tensor = n.attr['value'].tensor
+#             size = len(tensor.tensor_content)
+#             if size > max_const_size:
+#                 tensor.tensor_content = tf.compat.as_bytes("<stripped %d bytes>" % size)
+#     return strip_def
+#
+#
+# def rename_nodes(graph_def, rename_func):
+#     res_def = tf.GraphDef()
+#     for n0 in graph_def.node:
+#         n = res_def.node.add()
+#         n.MergeFrom(n0)
+#         n.name = rename_func(n.name)
+#         for i, s in enumerate(n.input):
+#             n.input[i] = rename_func(s) if s[0] != '^' else '^' + rename_func(s[1:])
+#     return res_def
+#
+#
+# def show_graph(graph_def, max_const_size=32):
+#     """Visualize TensorFlow graph."""
+#     if hasattr(graph_def, 'as_graph_def'):
+#         graph_def = graph_def.as_graph_def()
+#     strip_def = strip_consts(graph_def, max_const_size=max_const_size)
+#     code = """
+#         <script>
+#           function load() {{
+#             document.getElementById("{id}").pbtxt = {data};
+#           }}
+#         </script>
+#         <link rel="import" href="https://tensorboard.appspot.com/tf-graph-basic.build.html" onload=load()>
+#         <div style="height:600px">
+#           <tf-graph-basic id="{id}"></tf-graph-basic>
+#         </div>
+#     """.format(data=repr(str(strip_def)), id='graph' + str(np.random.rand()))
+#
+#     iframe = """
+#         <iframe seamless style="width:800px;height:620px;border:0" srcdoc="{}"></iframe>
+#     """.format(code.replace('"', '&quot;'))
+#     # display(HTML(iframe))
+#
+#
+# # Visualizing the network graph. Be sure expand the "mixed" nodes to see their
+# # internal structure. We are going to visualize "Conv2D" nodes.
+# tmp_def = rename_nodes(graph_def, lambda s: "/".join(s.split('_', 1)))
+# show_graph(tmp_def)
 
 # Picking some internal layer. Note that we use outputs before applying the ReLU nonlinearity
 # to have non-zero gradients for features with negative initial activations.
@@ -87,56 +85,63 @@ layer = 'mixed4d_3x3_bottleneck_pre_relu'
 channel = 139  # picking some feature channel to visualize
 
 # start with a gray image with a little noise
-img_noise = np.zeros(shape=(224, 224, 3)) + 100.0 #.random.uniform(size=(224, 224, 3)) + 100.0
+img_noise = np.zeros(shape=(224, 224, 3)) + 100.0  # .random.uniform(size=(224, 224, 3)) + 100.0
 
 cv2.namedWindow("Show", cv2.WND_PROP_FULLSCREEN)
 cv2.setWindowProperty("Show", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
 
 
-def showarray(a, fmt='jpeg'):
+def showarray(a):
     a = np.uint8(np.clip(a, 0, 1) * 255)
     cv2.imshow('Show', a)
     cv2.waitKey(1)
 
 
 def T(layer):
-    '''Helper for getting layer output tensor'''
+    # Helper for getting layer output tensor'''
     return graph.get_tensor_by_name("import/%s:0" % layer)
 
 
 def tffunc(*argtypes):
-    '''Helper that transforms TF-graph generating function into a regular one.
-    See "resize" function below.
-    '''
+    # Helper that transforms TF-graph generating function into a regular one.
+    # See "resize" function below.
+
     placeholders = list(map(tf.placeholder, argtypes))
+
     def wrap(f):
         out = f(*placeholders)
+
         def wrapper(*args, **kw):
             return out.eval(dict(zip(placeholders, args)), session=kw.get('session'))
+
         return wrapper
+
     return wrap
+
 
 # Helper function that uses TF to resize an image
 def resize(img, size):
     img = tf.expand_dims(img, 0)
-    return tf.image.resize_bilinear(img, size)[0,:,:,:]
+    return tf.image.resize_bilinear(img, size)[0, :, :, :]
+
+
 resize = tffunc(np.float32, np.int32)(resize)
 
 
 def calc_grad_tiled(img, t_grad, tile_size=200):
-    '''Compute the value of tensor t_grad over the image in a tiled way.
-    Random shifts are applied to the image to blur tile boundaries over
-    multiple iterations.'''
+    # '''Compute the value of tensor t_grad over the image in a tiled way.
+    # Random shifts are applied to the image to blur tile boundaries over
+    # multiple iterations.'''
     sz = tile_size
     h, w = img.shape[:2]
     sx, sy = np.random.randint(sz, size=2)
     img_shift = np.roll(np.roll(img, sx, 1), sy, 0)
     grad = np.zeros_like(img)
-    for y in range(0, max(h-sz//2, sz),sz):
-        for x in range(0, max(w-sz//2, sz),sz):
-            sub = img_shift[y:y+sz,x:x+sz]
-            g = sess.run(t_grad, {t_input:sub})
-            grad[y:y+sz,x:x+sz] = g
+    for y in range(0, max(h - sz // 2, sz), sz):
+        for x in range(0, max(w - sz // 2, sz), sz):
+            sub = img_shift[y:y + sz, x:x + sz]
+            g = sess.run(t_grad, {t_input: sub})
+            grad[y:y + sz, x:x + sz] = g
     return np.roll(np.roll(grad, -sx, 1), -sy, 0)
 
 
@@ -168,11 +173,12 @@ def render_deepdream(t_obj, img0=img_noise,
 
     return img
 
+
 def get_image(path):
     img0 = cv2.imread(path)
     img0 = np.float32(img0)
-    showarray(img0/255.0)
     return img0
+
 
 # img0 = get_image('ImagesIn/profile800.jpg')
 # render_deepdream(T(layer)[:,:,:,139], img0)
@@ -181,22 +187,29 @@ def get_image(path):
 img_profile = get_image('ImagesIn/profile740.jpg')
 img_parasol = get_image('ImagesIn/parasolSmall.jpg')
 img_eye = get_image('ImagesIn/eye740.jpg')
+img_leaves = get_image('ImagesIn/leaves.jpg')
+img_roses = get_image('ImagesIn/roses.jpg')
 
-channels = [1,16,18,7,24,4,11,19,23,111,30,36,31,42,41,44,1123,108,109,47,45,51,52,127,128,134,57,58,53,60,139,140,112,
-            61,75,101,100,98,90,136,114,122,115,82,70,138,97,116,117,87,141,86,83,143]
+channels = [1, 16, 18, 7, 24, 4, 11, 19, 23, 111, 30, 36, 31, 42, 41, 44, 123, 108, 109, 47, 45, 51, 52, 127, 128, 134,
+            57, 58, 53, 60, 139, 140, 112,
+            61, 75, 101, 100, 98, 90, 136, 114, 122, 115, 82, 70, 138, 97, 116, 117, 87, 141, 86, 83, 143]
 for channel in channels:
-    which_image = random.randint(1, 3)
+    which_image = random.randint(1, 5)
 
     if which_image == 1:
-        render_deepdream(T(layer)[:,:,:,channel], img_eye)
+        render_deepdream(T(layer)[:, :, :, channel], img_eye)
+        print('eye')
     elif which_image == 2:
         render_deepdream(T(layer)[:, :, :, channel], img_parasol)
+        print('parasol')
+    elif which_image == 3:
+        render_deepdream(T(layer)[:, :, :, channel], img_leaves)
+    elif which_image == 4:
+        render_deepdream(T(layer)[:, :, :, channel], img_roses)
     else:
         render_deepdream(T(layer)[:, :, :, channel], img_profile)
+        print('profile')
 
-
-
-    # path = 'ImagesOut/mixed' + str(i) + '.jpg'
-    # print(path)
-    # cv2.imwrite(path, img)
-
+        # path = 'ImagesOut/mixed' + str(i) + '.jpg'
+        # print(path)
+        # cv2.imwrite(path, img)
